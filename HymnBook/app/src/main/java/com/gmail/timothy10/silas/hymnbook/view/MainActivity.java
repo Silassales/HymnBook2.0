d8286fe689fbf409be7875eb8ad2d8d42855f882
Silas Sales <silas.timothy10@gmail.com>
2018-05-31 13:07:18 +0800

    removed sscore from mainActivity

--- HymnBook/app/src/main/java/com/gmail/timothy10/silas/hymnbook/view/MainActivity.java
+++ HymnBook/app/src/main/java/com/gmail/timothy10/silas/hymnbook/view/MainActivity.java
@@ -1,10 +1,5 @@
 package com.gmail.timothy10.silas.hymnbook.view;
 
-import java.util.ArrayList;
-
-import com.gmail.timothy10.silas.hymnbook.model.MXLparser;
-import com.gmail.timothy10.silas.hymnbook.model.ZipInputStreamImpl;
-
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -14,33 +9,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.widget.ScrollView;
-import uk.co.dolphin_com.sscore.SScore;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
-    /**
-     * the current magnification.
-     * <p>
-     * Preserved to avoid reload on rotate (which causes complete destruction and recreation of this Activity)
-     */
-    private float magnification;
-
-    /**
-     * the View which displays the score
-     */
-    private SeeScoreView ssview;
-
-    /**
-     * to parse the mxl files into xml files to be read by sscore
-     */
-    private MXLparser mxLparser;
-
-    /**
-     * ZipInputStreamUtil for unzipping mxl files into xml files to be read by seescore
-     */
-    private ZipInputStreamImpl zipInputStream = new ZipInputStreamImpl();
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,47 +36,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        //Creating the cursor view
-        final CursorView cursorView = new CursorView(this, new CursorView.OffsetCalculator() {
-            public float getScrollY() {
-                final ScrollView sv = findViewById(R.id.mainScrollView);
-                return sv.getScrollY();
-            }
-        });
-
-        ssview = new SeeScoreView(this, cursorView, getAssets(), new SeeScoreView.ZoomNotification(){
-
-            public void zoom(float scale) {
-                magnification = scale;
-            }
-
-        }, new SeeScoreView.TapNotification() {
-            public void tap(int systemIndex, int partIndex, int barIndex, Component[] components) {
-                System.out.println("tap system:" + systemIndex + " bar:" + barIndex);
-
-                for (Component comp : components)
-                    System.out.println(comp);
-            }
-
-            public void longTap(int systemIndex, int partIndex, int barIndex, Component[] components) {
-
-                invalidateOptionsMenu();
-            }
-        });
-
-        final ScrollView scrollView = findViewById(R.id.mainScrollView);
-        scrollView.addView(ssview);
-
-        mxLparser = new MXLparser(this);
-        SScore sScore = mxLparser.generateSScore(4);
-        ArrayList parts = new ArrayList<Boolean>();
-
-        for (int i = 0; i < sScore.numParts(); ++i)
-            parts.add(true);
-
-        ssview.setScore(sScore, parts, 0.5f);
-
     }
 
     @Override
@@ -166,11 +95,4 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    /**
-     * load the SeeScoreLib.so library
-     */
-    static {
-        System.loadLibrary("stlport_shared");
-        System.loadLibrary("SeeScoreLib");
-    }
 }
